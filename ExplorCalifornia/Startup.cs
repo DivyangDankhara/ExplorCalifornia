5ace58abc637a4161b9d9d98cce0c1cb75eef274
Divyang <Divyangdankhara007@outlook.com>
2019-12-07 18:30:40 -0500

    Added: configuration API to the project

--- ExplorCalifornia/Startup.cs
+++ ExplorCalifornia/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,12 @@ namespace ExplorCalifornia
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,18 +28,22 @@ namespace ExplorCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            app.UseExceptionHandler("/error.html");
+            
+            if (configuration.GetValue<bool>("EnableDeveloperException"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
+            app.UseFileServer();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapGet("/divyang", async (context) =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    throw new Exception("Error");
                 });
             });
         }
