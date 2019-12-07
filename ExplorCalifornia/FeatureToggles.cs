8dd46df323016baa962fadee51cc873e608bd702
Divyang <Divyangdankhara007@outlook.com>
2019-12-07 18:52:16 -0500

    Added Dependancie Injection while starting the project

---
+++ ExplorCalifornia/FeatureToggles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExplorCalifornia
+{
+    public class FeatureToggles
+    {
+        public bool DeveloperException  { get; set; }
+    }
+}
--- ExplorCalifornia/Startup.cs
+++ ExplorCalifornia/Startup.cs
@@ -23,15 +23,19 @@ namespace ExplorCalifornia
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                DeveloperException = configuration.GetValue<bool>("EnableDeveloperException")
+            });;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,FeatureToggles features)
         {
 
             app.UseExceptionHandler("/error.html");
             
-            if (configuration.GetValue<bool>("EnableDeveloperException"))
+            if (features.DeveloperException)
             {
                 app.UseDeveloperExceptionPage();
             }
