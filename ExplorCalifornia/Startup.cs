2d0172fc886e2f36ac47777856cb01499b78d33e
Divyang <Divyangdankhara007@outlook.com>
2019-12-12 22:50:34 -0500

    Final commit on the project.

--- ExplorCalifornia/Startup.cs
+++ ExplorCalifornia/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ExplorCalifornia.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace ExplorCalifornia
 {
@@ -47,6 +42,7 @@ namespace ExplorCalifornia
 
             services.AddIdentityCore<IdentityUser>().AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<IdentityDataContext>();
+
 
             services.AddMvc();
         }
